70a3fdc708b5fd23f62d7afee76c9f6cfa29787f
Michael Fay <michaelfay95@gmail.com>
2016-04-14 12:55:46 -0500

    Base Commit

---
+++ Communications/log.hpp
@@ -0,0 +1,50 @@
+/*****************************************************************************
+*********************Syncronization Library for Atmega 2560*******************
+******************************************************************************
+
+Developed By: Michael Fay for Rocksat-X 2016
+
+File Name:
+Last revised:
+Version Number:
+
+Tested By:
+Last Tested:
+******************************************************************************/
+#ifndef __LOG_H__
+#define __LOG_H__
+
+
+/******************************* Includes ************************************/
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#ifndef __PACKET_DEF_H__
+  #include "packet_def.hpp"
+#endif
+/*********************** Memory Constants ************************************/
+
+/***************** Macro's and Definitions************************************/
+
+
+/*******************Global Variables******************************************/
+
+
+/*********************** Function Proto's ************************************/
+
+
+//Function to replace printf.
+//print to log if not in PC_DEBUG MODE
+
+//function to handle file for log
+
+
+#endif
+/*********************** Note's **********************************************/
+/*
+
+
+
+
+*/
